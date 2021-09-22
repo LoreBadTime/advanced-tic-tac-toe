713a1a7e813b9313296e39d1d1c1e3530f1f1793
Lorenzo <concas324@gmail.com>
2021-09-22 13:54:25 +0200

    added (hard)offensive mode

--- windows/refact/main.py
+++ windows/refact/main.py
@@ -304,6 +304,229 @@ def gameplayhard(K,num):
                       cont = cont + 1
                       pl2.append(AI)
                       return cont,pl1,pl2,combo,combinations
+def gameplayhardattack(K,num):
+         global combinations,strategy,combo,cont,pl1,pl2,a,b,c,d,e,f,g,h,i,x,comb2,comb,AI,pl1list,pl2list,trick,ptot,fun
+         trick = 0
+         if cont == 1 or cont == 3 or cont == 5 or cont == 7 or cont == 9:
+                 pl1.append(num)
+                 pl1list = (list(permutations(pl1,2)))
+                 pl2list = (list(permutations(pl2,2)))
+                 cont = cont + 1
+                 AI = []
+                 strategy = True
+                 if cont < 9:
+                      if cont == 2:
+                           if 1 in pl2:
+                               if num == 2:#no
+                                   AI.append(7)
+                                   strategy = False
+                               elif num == 4:#no
+                                   AI.append(3)
+                                   strategy = False
+                               elif num == 3 or num == 7:#si
+                                   AI.append(9)
+                               elif num == 5:#no
+                                   AI.append(random.choice(list(6,8,9)))
+                                   strategy = False
+                               elif num == 6 or num == 8:#si
+                                   AI.append(5)
+                               elif num == 9:#no
+                                   AI.append(random.choice(list(3,7)))
+                                   strategy = False
+                           elif 3 in pl2:
+                               if num == 1 or num == 9:#si
+                                   AI.append(7)
+                               elif num == 2:#no
+                                   AI.append(9)
+                                   strategy = False
+                               elif num == 6:#no
+                                   AI.append(1)
+                                   strategy = False
+                               elif num == 5:#no
+                                   AI.append(random.choice(list(4,7,8)))
+                                   strategy = False
+                               elif num == 4 or num == 8:#si
+                                   AI.append(7)
+                               elif num == 7:#no
+                                   AI.append(random.choice(list(1,9)))
+                                   strategy = False
+                           elif 7 in pl2:
+                               if num == 4:
+                                   AI.append(9)
+                                   strategy = False
+                               elif num == 8:#no
+                                   AI.append(1)
+                                   strategy = False
+                               elif num == 1 or num == 9:#no
+                                   AI.append(3)
+                               elif num == 5:#no
+                                   AI.append(random.choice(list(2,3,6)))
+                                   strategy = False
+                               elif num == 2 or num == 6:#si
+                                   AI.append(5)
+                               elif num == 3:#no
+                                   AI.append(random.choice(list(1,9)))
+                                   strategy = False
+                           elif 9 in pl2:
+                               if num == 6:
+                                   AI.append(7)
+                                   strategy = False
+                               elif num == 8:#no
+                                   AI.append(3)
+                                   strategy = False
+                               elif num == 3 or num == 7:#si
+                                   AI.append(1)
+                               elif num == 5:#no
+                                   AI.append(random.choice(list(1,2,4)))
+                                   strategy = False
+                               elif num == 2 or num == 4:#si
+                                   AI.append(5)
+                               elif num == 1:#no
+                                   AI.append(random.choice(list(3,7)))
+                                   strategy = False
+                           elif 5 in pl2:
+                               if num == 2:
+                                   AI.append(random.choice(list(7,9)))
+                               elif num == 6:
+                                   AI.append(random.choice(list(7,1)))
+                               elif num == 4:
+                                   AI.append(random.choice(list(3,9)))
+                               elif num == 8:#si
+                                   AI.append(random.choice(list(3,1)))
+                               elif num == 1 :#no
+                                   AI.append(9)
+                                   strategy = False
+                               elif num == 3 :#no
+                                   AI.append(7)
+                                   strategy = False
+                               elif num == 7 :#no
+                                   AI.append(3)
+                                   strategy = False
+                               elif num == 9 :#no
+                                   AI.append(1)
+                                   strategy = False
+##                                
+##                      elif cont == 3 and (winck(pl1list,comb7) == True or winck(pl1list,comb6) == True or winck(pl1list,comb3) == True or winck(pl1list,comb5) == True): 
+##                           if winck(pl1list,comb3) == True:
+##                                AI.append(random.choice(comb1))
+##                                 
+##                           elif winck(pl1list,comb5) == True:
+##                                if check(pl1list,comb5[0]) == True:
+##                                     AI.append(9)
+##                                     if winck(pl1,AI) == True or winck(pl2,AI) == True:
+##                                          AI.remove(9)
+##                                          AI.append(3)
+##                                elif check(pl1list,comb5[1]) == True:
+##                                     AI.append(1)
+##                                     if winck(pl1,AI) == True or winck(pl2,AI) == True:
+##                                          AI.remove(1)
+##                                          AI.append(7)
+##                                elif check(pl1list,comb5[2]) == True:
+##                                     AI.append(7)
+##                                     if winck(pl1,AI) == True or winck(pl2,AI) == True:
+##                                          AI.remove(7)
+##                                          AI.append(9)
+##                                elif check(pl1list,comb5[3]) == True:
+##                                     AI.append(3)
+##                                     if winck(pl1,AI) == True or winck(pl2,AI) == True:
+##                                          AI.append(1)
+##                                          AI.remove(3)
+##                             
+##                           elif winck(pl1list,comb7) == True:
+##                                if check(pl1list,comb7[0]) == True:
+##                                     AI.append(3)
+##                                elif check(pl1list,comb7[1]) == True:
+##                                     AI.append(9)
+##                                elif check(pl1list,comb7[2]) == True:
+##                                     AI.append(7)
+##                                elif check(pl1list,comb7[3]) == True:
+##                                     AI.append(1)
+##
+##                                    
+##                           elif winck(pl1list,comb6) == True:
+##                                if check(pl1list,comb6[0]) == True or check(pl1list,comb6[4]) == True :
+##                                     AI.append(4)
+##                                elif check(pl1list,comb6[1]) == True or check(pl1list,comb6[2]) == True:
+##                                     AI.append(2)
+##                                elif check(pl1list,comb6[3]) == True or check(pl1list,comb6[6]) == True :
+##                                     AI.append(6)
+##                                elif check(pl1list,comb6[5]) == True or check(pl1list,comb6[7]) == True:
+##                                     AI.append(8)
+##                                
+##                                combinations = True
+##                           
+##                      elif cont == 5 and combinations == True:
+##                           
+##                           if check(pl1list,comb6[0]) == True or check(pl1list,comb6[1]) == True :
+##                                AI.append(9)
+##                           elif check(pl1list,comb6[2]) == True or check(pl1list,comb6[3]) == True:
+##                                AI.append(7)
+##                           elif check(pl1list,comb6[4]) == True or check(pl1list,comb6[5]) == True:
+##                                AI.append(3)
+##                           elif check(pl1list,comb6[6]) == True or check(pl1list,comb6[7]) == True :
+##                                AI.append(1)    
+                      if (winck(comb2,pl1list) == True or winck(comb2,pl2list) == True) and strategy == True :
+                          speedwrite(0,3)
+                          speedwrite(1,2)
+                          speedwrite(2,1)
+                          speedwrite(3,6)
+                          speedwrite(4,4)
+                          speedwrite(5,5)
+                          speedwrite(6,9)
+                          speedwrite(7,8)
+                          speedwrite(8,7)
+                          speedwrite(9,7)
+                          speedwrite(10,4)
+                          speedwrite(11,1)
+                          speedwrite(12,8)
+                          speedwrite(13,2)
+                          speedwrite(14,5)
+                          speedwrite(15,9)
+                          speedwrite(16,3)
+                          speedwrite(17,6)
+                          speedwrite(18,9)
+                          speedwrite(19,1)
+                          speedwrite(20,5)
+                          speedwrite(21,7)
+                          speedwrite(22,5)
+                          speedwrite(23,3)
+                          
+                          
+                      try:
+                          for x in AI:#this will eliminate duplicates and will prefer AI wins
+                                    x = int(x)
+                                    pl1a = pl1
+                                    pl2b = pl2
+                                    pl1a.append(x)
+                                    pl2b.append(x)
+                                    pl1test = (list(permutations(pl1a,3)))
+                                    pl2test = (list(permutations(pl2b,3)))
+                                    pl1a.remove(x)
+                                    pl2b.remove(x)
+                                    if winck(win,pl2test) == True and check(pl1,x) != True:
+                                         AI = []
+                                         AI.append(x)
+                                         break
+                                    elif winck(win,pl1test) == True and check(pl2,x) != True:
+                                         AI = x
+                          if check(pl1,AI) == True or check(pl2,AI) == True or AI == []:   
+                                       AI = int(randint(1, 10, 1))
+                                       while check(pl1,AI) == True or check(pl2,AI) == True:
+                                            AI = int(randint(1, 10, 1))
+                      except:
+                          pass
+                      try:
+                          AI = str(AI)[1]
+                          AI = int(AI)
+                      except:
+                          pass
+                      else:
+                           if cont < 9:
+                                while check(pl1,AI) == True or check(pl2,AI) == True or AI == []:
+                                       AI = int(randint(1, 10, 1))
+                      cont = cont + 1
+                      pl2.append(AI)
+                      return cont,pl1,pl2,combo,combinations
 def display():
          global pl1,pl2
          for y in pl1:
@@ -348,7 +571,7 @@ def display():
 
          return cont,pl1,pl2,combo,combinations
 def callback(K,num,mode):#already tryed outside
-         global combo,cont,Vittoria,co,color,pl1vitt,pl2vitt,turn,btn,a,b,c,d,e,f,g,h,i,x,ptot,AI,pl1win,pl2win,turner
+         global turner,combo,cont,Vittoria,co,color,pl1vitt,pl2vitt,turn,btn,a,b,c,d,e,f,g,h,i,x,ptot,AI,pl1win,pl2win,turner
          AI = []
          combo = False
          #disable btn when pressed more times and prevent other clicks
@@ -356,12 +579,15 @@ def callback(K,num,mode):#already tryed outside
                  cont = -2
          if check(pl1,num) == True or check(pl2,num) == True:
              K.config(command=0)
-         #normal game (i separated it for other propouses)
          else:
              if mode == 1:
                  gameplaynormal(K,num)
              elif mode == 2:
+               if (turner % 2) == 0:
                  gameplayhard(K,num)
+               else:
+                 gameplayhardattack(K,num)
+                 
              elif mode == 3:
                  gameplayeasy(K,num)
              display()
@@ -532,11 +758,11 @@ def start(l,a1,a2,mode): #main play and reset values for win possibilities
              i = Button(master, text="",font=('arial','20'), state=ACTIVE, height = 1,width = 3,activebackground=color,background=color)
              i.configure(command=lambda :callback(i,9,mode))
              i.grid(row=2 ,column=2)
-             #needs developing attack and defense mode
-             if ((turner % 2) == 1) and (mode == 1 or mode == 3):
+             #needs developing attack mode
+             if ((turner % 2) == 1):
                 tmpAI = 0
-                if mode == 1:##or mode == 2:
-                    tmpAI = randint(1, 5, 1)
+                if mode == 1 or mode == 2:
+                    tmpAI = randint(1, 6, 1)
                     if tmpAI == 1:
                          pass
                     elif tmpAI == 2:
